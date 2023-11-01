ded05a23ef33aacdabc950ae7267985e5d1e45ab
metalurgical <97008724+metalurgical@users.noreply.github.com>
2023-11-01 10:57:20 +0200

    fix: lock secp256k1 to 0.12.2

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
             targets: ["SessionManager"])
     ],
     dependencies: [
+        .package(name: "secp256k1", url: "https://github.com/GigaBitcoin/secp256k1.swift.git", .exact( "0.12.2")),
         .package(name: "KeychainSwift", url: "https://github.com/evgenyneu/keychain-swift.git", from: "20.0.0"),
         .package(name: "web3.swift", url: "https://github.com/argentlabs/web3.swift", from: "1.6.0"),
         .package(name: "CryptoSwift", url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.8.0")
@@ -19,7 +20,7 @@ let package = Package(
     targets: [
         .target(
             name: "SessionManager",
-            dependencies: ["KeychainSwift", "web3.swift", "CryptoSwift"]),
+            dependencies: ["KeychainSwift", "web3.swift", "secp256k1", "CryptoSwift"]),
         .testTarget(
             name: "SessionManagerTests",
             dependencies: ["SessionManager"])
